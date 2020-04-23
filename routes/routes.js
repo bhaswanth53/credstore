3c253c37c708143eea1e404375cd935f539871ae
Bhaswanth53 <bhaswanth22@gmail.com>
2020-04-23 10:25:22 +0530

    created user dashboard

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const SiteController = require("../controllers/site")
 const AuthController = require("../controllers/auth")
+const UserController = require("../controllers/user")
 
 const router = express.Router()
 
@@ -10,5 +11,8 @@ router.get("/", SiteController.homepage)
 router.get("/login", AuthController.login)
 router.get("/register", AuthController.register)
 router.get("/forgot-password", AuthController.forgot)
+
+// User Routes
+router.get("/user/dashboard", UserController.dashboard)
 
 module.exports = router
